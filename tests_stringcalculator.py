072860252914094e83810123ce7384a56299fb88
JoelSolano59 <joel.solano59@gmail.com>
2022-10-01 15:20:20 -0500

    test

--- tests_stringcalculator.py
+++ tests_stringcalculator.py
@@ -12,6 +12,9 @@ class TestStringMethods(unittest.TestCase):
     def test_one_comma_two_args(self):
         self.assertEqual(stringcalculator.Add("1,2"), 3)
 
+    def test_jump_line_args(self):
+        self.assertEqual(stringcalculator.Add("1\n2,3"), 6)
+
 
 if __name__ == "__main__":
     unittest.main()
