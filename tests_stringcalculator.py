97e2ff465d09d2313aaf096f1b765de3b9ec51d0
JoelSolano59 <joel.solano59@gmail.com>
2022-10-01 15:33:14 -0500

    test

--- tests_stringcalculator.py
+++ tests_stringcalculator.py
@@ -15,6 +15,9 @@ class TestStringMethods(unittest.TestCase):
     def test_jump_line_args(self):
         self.assertEqual(stringcalculator.Add("1\n2,3"), 6)
 
+    def test_custom_delimeter_args(self):
+        self.assertEqual(stringcalculator.Add(";\n1;2"), 3)
+
 
 if __name__ == "__main__":
     unittest.main()
